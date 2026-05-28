3e62a89c6006993102d1529bcefa5275093a781d
vonner04 <randyleean092m@gmail.com>
2026-05-29 05:25:58 +1200

    feat: added lock shake effect when claiming a locked badge
    fix: same rotate + scale effect when hoverin on a clickable button
    
    refactor: adjusted colors and font to match canva design.

--- components/achievements/achievement-progress-bar.tsx
+++ components/achievements/achievement-progress-bar.tsx
@@ -7,13 +7,13 @@ export default function AchievementProgressBar({ current, required }: Achievemen
   const progressPercent = Math.min((current / required) * 100, 100);
 
   return (
-    <div className='relative h-9 w-full overflow-hidden rounded-full bg-gray-200'>
+    <div className='relative h-12 w-full overflow-hidden rounded-full bg-gray-200'>
       <div
         className='h-full rounded-full bg-green-400 transition-all duration-300'
         style={{ width: `${progressPercent}%` }}
       />
 
-      <p className='absolute inset-0 flex items-center justify-center text-lg font-black text-black'>
+      <p className='absolute inset-0 flex items-center justify-center text-lg font-black text-black font-pixel'>
         {current} / {required}
       </p>
     </div>
--- components/achievements/achievement-row.tsx
+++ components/achievements/achievement-row.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Eye, Gift, Lock } from "lucide-react";
 import AchievementBadge from "./achievement-badge";
 import AchievementProgressBar from "./achievement-progress-bar";
@@ -11,58 +12,88 @@ type AchievementRowProps = {
 };
 
 export default function AchievementRow({ achievement, onClaim, onView }: AchievementRowProps) {
+  const [isLockShaking, setIsLockShaking] = useState(false);
+
   const isClaimable = canClaimAchievement(achievement);
 
   const title = getAchievementTitle(achievement);
 
+  function handleClaimClick() {
+    if (!achievement.isUnlocked) {
+      setIsLockShaking(true);
+
+      window.setTimeout(() => {
+        setIsLockShaking(false);
+      }, 400);
+
+      return;
+    }
+
+    if (!isClaimable) return;
+
+    onClaim(achievement.id);
+  }
   return (
     <div
       className='
         flex w-full max-w-9/10 items-center gap-5
-        rounded-4xl bg-yellow-950/20 p-5
+        rounded-4xl bg-[#d3c399]/50
         shadow-sm backdrop-blur-[1px]
       '
     >
-      <div className='rounded-3xl bg-white/25 p-3'>
+      <div className='rounded-3xl bg-[#d6d0c2] p-3'>
         <AchievementBadge badgeSrc={achievement.badgeSrc} isUnlocked={achievement.isUnlocked} />
       </div>
 
-      <div className='flex flex-1 flex-col gap-5'>
+      <div className='flex flex-1 flex-col gap-5 px-8'>
         <h2
           className='
-            text-center text-5xl font-black uppercase tracking-wider
+            font-pixel
+            text-center text-4xl font-black uppercase tracking-wider
             text-purple-500
             drop-shadow-[4px_4px_0px_white]
-            '
+          '
         >
           {title}
         </h2>
 
         <AchievementProgressBar current={achievement.currentProgress} required={achievement.requiredProgress} />
 
-        <div className='grid grid-cols-[2fr_1fr] gap-3'>
+        <div className='grid grid-cols-[2fr_1fr] gap-4'>
           <button
             type='button'
-            disabled={!isClaimable}
-            onClick={() => onClaim(achievement.id)}
+            onClick={handleClaimClick}
+            disabled={achievement.isUnlocked && !isClaimable}
             className={`
-                group flex items-center justify-center rounded-lg px-6 py-4
-                text-2xl font-black uppercase text-white
-                transition active:scale-95
-                ${isClaimable ? "bg-emerald-500 hover:bg-emerald-600" : "cursor-not-allowed bg-yellow-950/20"}
+              group flex items-center justify-center rounded-lg py-3
+              text-2xl font-black uppercase text-white
+              transition active:scale-95
+              ${
+                !achievement.isUnlocked
+                  ? "bg-yellow-950/20 text-white/70 hover:bg-yellow-950/30"
+                  : isClaimable
+                    ? "bg-emerald-500 hover:bg-emerald-600"
+                    : "cursor-not-allowed bg-yellow-950/20 text-gray-500"
+              }
             `}
           >
             {!achievement.isUnlocked ? (
               <Lock
                 size={34}
                 strokeWidth={2}
-                className=' transition-transform duration-150 group-active:-translate-x-1 group-active:rotate-12'
+                className={`
+                  transition-transform duration-200
+                  ${isLockShaking ? "animate-lock-shake" : ""}
+                `}
               />
             ) : (
               <Gift
                 size={34}
                 strokeWidth={2}
-                className='transition-transform duration-200 group-hover:scale-125 group-hover:-rotate-6'
+                className='
+                  transition-transform duration-200
+                  group-hover:scale-125 group-hover:-rotate-6
+                '
               />
             )}
           </button>
@@ -71,12 +102,17 @@ export default function AchievementRow({ achievement, onClaim, onView }: Achieve
             type='button'
             onClick={() => onView(achievement)}
             className='
-              flex items-center justify-center rounded-md
-              bg-purple-700 px-4 py-2 text-white
+              group flex items-center justify-center rounded-md
+              bg-(--color-primary) px-4 py-2 text-white
               transition hover:bg-purple-800 active:scale-95
             '
           >
-            <Eye size={32} strokeWidth={3} />
+            <Eye
+              size={32}
+              strokeWidth={3}
+              className='transition-transform duration-200
+                  group-hover:scale-125 group-hover:-rotate-6'
+            />
           </button>
         </div>
       </div>
